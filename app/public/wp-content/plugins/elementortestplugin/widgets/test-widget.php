30345958337ce472a07a6c85a1fe49ea8b1c5d25
Morshed <skmorshed75@yahoo.com>
2020-04-07 22:37:13 +0600

    ১.৬ - এলিমেন্টর কন্ট্রোলের খুবই পাওয়ারফুল একটা অপশন 'সিলেকটর' কিভাবে ব্যবহার করতে হয়

--- app/public/wp-content/plugins/elementortestplugin/widgets/test-widget.php
+++ app/public/wp-content/plugins/elementortestplugin/widgets/test-widget.php
@@ -36,12 +36,23 @@ class Elementor_Test_Widget extends \Elementor\Widget_Base {
 		$this->add_control(
 			'heading',
 			[
-				'label' => __( 'Type Something', 'eltp' ),
+				'label' => __( 'Heading', 'eltp' ),
 				'type' => \Elementor\Controls_Manager::TEXT,
 				'input_type' => 'text',
-				'placeholder' => __( 'Type your text', 'eltp' ),
+				'placeholder' => __( 'Type your heading', 'eltp' ),
 			]
 		);
+		//class 1.6
+		$this->add_control(
+			'heading_description',
+			[
+				'label' => __( 'Heading Description', 'eltp' ),
+				'type' => \Elementor\Controls_Manager::TEXTAREA,
+				'input_type' => 'text',
+				'placeholder' => __( 'Type your Description', 'eltp' ),
+			]
+		);
+		// end 1.6
 		$this->end_controls_section();
 
 		//Class 1.5
@@ -63,18 +74,80 @@ class Elementor_Test_Widget extends \Elementor\Widget_Base {
 					'right' => __( 'Right', 'eltp' ),
 					'center' => __( 'Center', 'eltp' ),
 				],
+				//Class 1.6
+				'selectors' =>[
+					'{{WRAPPER}} h1.heading' => 'text-align:{{VALUE}}'
+				],
+				//End Class 1.6
+			]
+		);
+		//class 1.6
+		$this->add_control(
+			'description_alignment',
+			[
+				'label' => __( 'Description Alignment', 'eltp' ),
+				'type' => \Elementor\Controls_Manager::SELECT,
+				'default' => 'center',
+				'options' => [
+					'left'  => __( 'Left', 'eltp' ),
+					'right' => __( 'Right', 'eltp' ),
+					'center' => __( 'Center', 'eltp' ),
+				],
+				//Class 1.6
+				'selectors' =>[
+					'{{WRAPPER}} p.description' => 'text-align:{{VALUE}}'
+				],
+				//End Class 1.6
+			]
+		);
+		//class 1.6
+		$this->end_controls_section();
 
+
+		//Class 1.6
+		$this->start_controls_section(
+			'color_section',
+			[
+				'label' => __('Color', 'eltp'),
+				'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
+			]
+		);		
+		$this->add_control(
+			'heading_color',
+			[
+				'label' => __( 'Heading Color', 'eltp' ),
+				'type' => \Elementor\Controls_Manager::COLOR,
+				'default' => '#224400',
+				'selectors' =>[
+					'{{WRAPPER}} h1.heading' => 'color: {{VALUE}}',
+				]
 			]
 		);
 
-		$this->end_controls_section();
+		$this->add_control(
+			'description_color',
+			[
+				'label' => __( 'Description Color', 'eltp' ),
+				'type' => \Elementor\Controls_Manager::COLOR,
+				'default' => '#666666',
+				'selectors' =>[
+					'{{WRAPPER}} p.description' => 'color: {{VALUE}}',
+				]
+			]
+		);
+		//End class 1.6		
+		$this->end_controls_section();		
 	}
 
 	protected function render() {
 		$settings = $this->get_settings_for_display();
 		$heading = $settings['heading'];
-		$alignment = $settings['alignment'];
-		echo "<h1 style = 'text-align:".esc_attr($alignment)."'>".esc_html($heading)."</h1>";
+		$heading_description = $settings['heading_description'];
+		//$alignment = $settings['alignment'];
+		//echo "<h1 style = 'text-align:".esc_attr($alignment)."'>".esc_html($heading)."</h1>";
+		//Class 1.6
+		echo "<h1 class='heading'>".esc_html($heading)."</h1>";
+		echo "<p class='description'>".wp_kses_post($heading_description)."</p>";
 	}
 
 	protected function _content_template() {}
